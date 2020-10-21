ad3f38b358961896fb51069d0312a16e3d59d79d
BNK8991 <nikolaibui1998@gmail.com>
2020-10-22 01:26:21 +0700

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-const express = require('express');                 //framework
-const engines = require('consolidate');             //thuc thi cac doan code js der chuyen ve ngon ngu may tinh co the hieu va doc duoc
+const express = require('express');                                             //framework
+const engines = require('consolidate');                                         //thuc thi cac doan code js der chuyen ve ngon ngu may tinh co the hieu va doc duoc
 const app = express();
 
 
@@ -10,7 +10,7 @@ app.set('views', './views');
 app.set('view engine', 'hbs');
 
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));            //mot doi tuong body chua du lieu ma da duoc phan tich cu pham se duoc dua vao request. du lieu do la cap key-value, trong do co the la true, array, string neeu extended false, cac loai con lai la true
+app.use(bodyParser.urlencoded({ extended: false }));                            //mot doi tuong body chua du lieu ma da duoc phan tich cu pham se duoc dua vao request. du lieu do la cap key-value, trong do co the la true, array, string neeu extended false, cac loai con lai la true
 
 var publicDir = require('path').join(__dirname, '/public');
 app.use(express.static(publicDir));
@@ -18,7 +18,7 @@ app.use(express.static(publicDir));
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb+srv://hoaison2204:Son22042000@cluster0.cnlb1.mongodb.net/test';
 
-app.get('/', async function (req, res) {                        //async khai bao ham bat dong bo - await tam dung ham bat dong bo
+app.get('/', async function (req, res) {                                        //async khai bao ham bat dong bo - await tam dung ham bat dong bo
     let client = await MongoClient.connect(url);
     let dbo = client.db("GCH0719");
     let results = await dbo.collection("Product").find({}).toArray();
@@ -39,6 +39,9 @@ app.get('/delete', async function (req, res) {
 app.get('/add', function (req, res) {
     res.render('updateSanPham');
 })
+app.get('/login', function (req, res) { //cái này chỉ để cho đẹp thôi chứ không login được. Tạo ra chỉ để đưa cái trang login vào để chụp rồi cho vào báo cáo
+    res.render('login');
+})
 
 app.post('/doAddproduct', async function (req, res) {
     let client = await MongoClient.connect(url);
@@ -49,7 +52,7 @@ app.post('/doAddproduct', async function (req, res) {
     let name = req.body.txtName;
     let price = req.body.txtPrice;
 
-    let newProduct = { _id : _id, name: name, price: price };
+    let newProduct = { _id: _id, name: name, price: price };
     await dbo.collection("Product").insertOne(newProduct);
     // console.log(newProduct);
 
